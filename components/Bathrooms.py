ad22468f81f7c81c1b298120566df99a111810dc
lestersanjuan <lsanjua1@uci.edu>
2022-11-05 15:37:04 -0700

    Added the implementation of Bathrooms and Floor

---
+++ components/Bathrooms.py
@@ -0,0 +1,12 @@
+import uuid
+from app import db
+from Base import Base
+from sqlalchemy.dialects.postgresql import UUID
+
+class Bathrooms(Base):
+    __tablename__ = 'Bathroom'
+    bathroom_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name_of_building = db.Column(db.String())
+    reivew_id = db.Column(UUID(as_uuid=True))
+    flood_number = db.column(db.Integer())
+    direction = db.column(db.String())
